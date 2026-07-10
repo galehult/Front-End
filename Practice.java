a787ad0c267d4823de40a2cc898e3bcbecab4ae4
galehult <josephmichaelabellanosa@gmail.com>
2026-07-10 17:28:12 +0800

    Practice Java

--- Practice.java
+++ Practice.java
@@ -1,4 +1,5 @@
-// import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Practice {
     public static void printStars(int number) {
@@ -6,21 +7,6 @@ public class Practice {
             System.out.print("*");
         }
         System.out.println("");
-    }
-
-    public static void printSpaces(int number) {
-        for (int i = 0; i < number; i++) {
-            System.out.print(" ");
-        }
-    }
-
-    public static void printTriangle(int size) {
-        int spaceCounter = size - 1;
-        for (int i = 1; i <= size + 3; i+=2) {
-            printSpaces(spaceCounter);
-            printStars(i);
-            spaceCounter--;
-        }
     }
 
     public static void printSquare(int size) {
@@ -37,18 +23,60 @@ public class Practice {
         }
     }
 
-    public static void christmasTree(int height) {
-        printTriangle(height);
-        for (int i = 0; i < 2; i++) {
-            printSpaces(height - 2);
-            printRectangle(3, 1);
+    public static void printTriangle(int size) {
+        int count = 0;
+        while (count < size) {
+            count++;
+            printStars(count);
         }
     }
 
-    public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
+    public static void smallestNumber() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        // printTriangle(4);
-        christmasTree(15);
+        int numInput = Integer.valueOf(scanner.nextLine());
+
+        while (numInput != 9999) {
+            numbers.add(numInput);
+            numInput = Integer.valueOf(scanner.nextLine());
+        }
+
+        int smallestNum = numbers.get(0);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < smallestNum) {
+                smallestNum = numbers.get(i);
+            }
+        }
+
+        System.out.println("Smallest number: " + smallestNum);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == smallestNum) {
+                System.out.println("Fount at index: " + i);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Printing Stars: ");
+        printStars(5);
+        printStars(3);
+        printStars(9);
+
+        System.out.println("This is a Square: ");
+        printSquare(4);
+        System.out.println();
+        printSquare(5);
+
+        System.out.println("This is a Rectangle: ");
+        printRectangle(17, 3);
+
+        System.out.println("This is a Triangle: ");
+        printTriangle(4);
     }
 }
