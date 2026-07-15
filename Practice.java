ca8fb3c7a28664afe564580a0d3302d2878d671c
galehult <josephmichaelabellanosa@gmail.com>
2026-07-15 17:59:20 +0800

    Java Testing

--- Practice.java
+++ Practice.java
@@ -1,16 +1,24 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> strings = new ArrayList<>();
+        // Scanner scanner = new Scanner(System.in);
+        // ArrayList<String> strings = new ArrayList<>();
 
-        Account myAccount = new Account("My Account", 100.00);
+        Debt mortgage = new Debt(120000.0, 1.20);
+        mortgage.printBalance();
 
-        myAccount.deposit(20);
-        System.out.println("New Balance: " + myAccount.balance());
-        System.out.println(myAccount);
+        mortgage.waitOneYear();
+        mortgage.printBalance();
 
+        int years = 0;
+
+        while (years < 20) {
+            mortgage.waitOneYear();
+            years = years + 1;
+        }
+
+        mortgage.printBalance();
     }
 }
